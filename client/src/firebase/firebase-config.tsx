6aada2a70c73415cf4e656c7c4541b3d0d63c2f1
Salt Developer <salt-dev@DEV045.localdomain>
2022-04-01 09:14:47 +0200

    firebase credentials

--- client/src/firebase/firebase-config.tsx
+++ client/src/firebase/firebase-config.tsx
@@ -9,14 +9,14 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-console.log(window.env.FIREBASE_API_KEY)
+console.log(process.env.FIREBASE_API_KEY)
 const firebaseConfig = {
-  apiKey: window.env.FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "pet-me-up.firebaseapp.com",
   projectId: "pet-me-up",
   storageBucket: "pet-me-up.appspot.com",
   messagingSenderId: "907869999421",
-  appId: window.env.FIREBASE_APP_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-BK22DDSGTX"
 };
 
