3d0ef386f029390ec7acdbb54b10378b1cedbea4
Salt Developer <salt-dev@DEV045.local>
2022-04-01 07:28:55 +0200

    firebase env

--- client/src/firebase/firebase-config.tsx
+++ client/src/firebase/firebase-config.tsx
@@ -11,12 +11,12 @@ import env from 'react-dotenv'
 
 console.log(env)
 const firebaseConfig = {
-  apiKey: env.FIREBASE_API_KEY,
+  apiKey: env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "pet-me-up.firebaseapp.com",
   projectId: "pet-me-up",
   storageBucket: "pet-me-up.appspot.com",
   messagingSenderId: "907869999421",
-  appId: env.FIREBASE_APP_ID,
+  appId: env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-BK22DDSGTX"
 };
 
