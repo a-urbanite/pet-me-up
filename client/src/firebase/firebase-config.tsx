68c343e29c2bd49d86bccbe2e75d66c0b26aecf1
a.urbanite <a.urbanite@gmail.com>
2023-06-20 17:12:17 +0200

    adjusted config var consume in firebase config

--- client/src/firebase/firebase-config.tsx
+++ client/src/firebase/firebase-config.tsx
@@ -5,12 +5,12 @@ import env from 'react-dotenv'
 
 
 const firebaseConfig = {
-  apiKey: env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "pet-me-up.firebaseapp.com",
   projectId: "pet-me-up",
   storageBucket: "pet-me-up.appspot.com",
   messagingSenderId: "907869999421",
-  appId: env.REACT_APP_FIREBASE_APP_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-BK22DDSGTX"
 };
 
